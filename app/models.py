e8478d16cdbd6da442d3bf528fc2f400d93757d2
scotte0626 <scotte0626@gmail.com>
2024-12-12 15:40:39 +0800

    Add something what i dont know

--- app/models.py
+++ app/models.py
@@ -9,3 +9,10 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, default=False)
     due_date = Column(Date, nullable=True)
+
+class User(Base):
+    __tablename__="users"
+    id=Column(Integer, primary_key=True,index=True)
+    username=Column(string(100),unique=True,nullable=False)
+    password=Column(string(100),nullable=False)
+    email=Column(string(100),unique=True,nullable=False)
